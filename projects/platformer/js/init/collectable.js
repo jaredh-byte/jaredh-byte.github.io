9c5ef3d0211c92dfad9ab9f1e7a2880e2b5d28bb
jaredh-byte <jaredhalldorson@gmail.com>
2020-10-01 20:09:28 +0000

    add index.html file

---
+++ projects/platformer/js/init/collectable.js
@@ -0,0 +1,55 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let collectable = window.opspark.collectable;
+
+    let type = {
+        db: {assetKey: 'db', points: 10},
+        max: {assetKey: 'max', points: 20},
+        steve: {assetKey: 'steve', points: 30},
+        grace: {assetKey: 'grace', points: 40},
+        kennedi: {assetKey: 'kennedi', points: 50}
+    };
+    
+    /**
+     * init: Initialize all collectables.
+     * 
+     * GOAL: Add as many collectables as necessary to make your level challenging.
+     * 
+     * Use the createCollectable() Function to create collectables for the level.
+     * See the type Object, above, for the types of collectables and their point values.
+     * 
+     * createCollectable() takes these arguments:
+     *      
+     *      createCollectable(type, x, y, gravity, bounce);
+     * 
+     *      type: The type of the collectable, use the type Object above.
+     *      x: The x coordineate for the collectable.
+     *      y: The y coordineate for the collectable.
+     *      gravity: OPTIONAL The gravitational pull on the collectable.
+     *      bounce: OPTIONAL A factor effecting how much the collectable will bounce off platforms, etc.
+     */ 
+    function init(game) {
+        let createCollectable = collectable.create;
+
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        createCollectable(type.steve, 200, 170, 6,);
+        
+        createCollectable(type.kennedi, 250, 72, 20)
+
+        createCollectable(type.kennedi, 600, 450, 29)
+
+        createCollectable(type.steve, 400, 109)
+
+        createCollectable(type.steve, 400, 109)
+
+        createCollectable(type.db, 600, 450)
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+    collectable.init = init;
+})(window);
